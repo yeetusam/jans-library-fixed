1b632195e7761e1f71fa5144f39f8b46bfac6473
stxchox <105161989+yeetusam@users.noreply.github.com>
2024-06-11 19:11:07 +0200

    Update linoria_cursorless.lua

--- linoria_cursorless.lua
+++ linoria_cursorless.lua
@@ -30,9 +30,9 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
+    MainColor = Color3.fromRGB(130, 155, 215);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 85, 255);
+    AccentColor = Color3.fromRGB(130, 155, 215);
     OutlineColor = Color3.fromRGB(50, 50, 50);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
